d7a1f39d184b126b4f24cf86812a5a458dde3be6
r5by <dllizhongwei@gmail.com>
2024-09-02 17:17:48 +0800

    add some tests

---
+++ tests/and.cpp
@@ -0,0 +1,13 @@
+#include "and.h"
+
+int bitwise_and(int a, int b) {
+    return a & b;
+}
+
+// int main() {
+//     int x = 5;  // 0b0101
+//     int y = 3;  // 0b0011
+//     int result = bitwise_and(x, y);
+
+//     return result;  // Should return 1 (0b0001)
+// }
---
+++ tests/and.h
@@ -0,0 +1,6 @@
+#ifndef AND_H
+#define AND_H
+
+int bitwise_and(int a, int b);
+
+#endif // AND_H
